827eafd9ccee41cb9a48fbe8c945456c0a1fd328
Daniel Culpepper <culpepperd2490@gmail.com>
2023-09-02 15:30:56 -0400

    added functions and passenger objects

---
+++ chp10Flying.js
@@ -0,0 +1,32 @@
+// passengers variable references an array of passenger objects
+var passengers = [ { name: "Jane Forloop", paid: true },
+                   { name: "Dr. Evel", paid: true },
+                   { name: "Sue Property", paid: false },
+                   { name: "John Funcall", paid: true } ];
+
+function processPassengers(passengerArray, testFunction) {
+    for (var i = 0; i < passengerArray.length; i++){
+        if(testFunction(passengerArray[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function checkNoFlyList(passengerObj) {
+    return (passengerObj.name === "Dr. Evel");
+}
+
+function checkNotPaid(passengerObj) {
+    return (!passengerObj.paid);
+}
+
+var allCanFly = processPassengers(passengers, checkNoFlyList);
+if(!allCanFly) {
+    console.log("The plane can't take off: we have a passenger on the no-fly list.");
+}
+
+var allPaid = processPassengers(passengers, checkNotPaid);
+if(!allPaid) {
+    console.log("The plane can't take off: not everyone has paid.");
+}
