a7c134d94d8825b2cc83f48649927d2b6443ed5a
Daniel Culpepper <culpepperd2490@gmail.com>
2023-09-03 14:17:06 -0400

    added functions to serve passenger drinks

--- chp10Flying.js
+++ chp10Flying.js
@@ -1,9 +1,10 @@
 // passengers variable references an array of passenger objects
-var passengers = [ { name: "Jane Forloop", paid: true },
-                   { name: "Dr. Evel", paid: true },
-                   { name: "Sue Property", paid: false },
-                   { name: "John Funcall", paid: true } ];
+var passengers = [ { name: "Jane Forloop", paid: true, ticket: "coach" },
+                   { name: "Dr. Evel", paid: true, ticket: "firstClass" },
+                   { name: "Sue Property", paid: false, ticket: "premium" },
+                   { name: "John Funcall", paid: true, ticket: "coach" } ];
 
+// main function to Process Passengers
 function processPassengers(passengerArray, testFunction) {
     for (var i = 0; i < passengerArray.length; i++){
         if(testFunction(passengerArray[i])) {
@@ -13,12 +14,50 @@ function processPassengers(passengerArray, testFunction) {
     return true;
 }
 
+// function to create a drink order dependent on ticket type
+function createDrinkOrder(passenger) {
+    var orderFunction;
+    if (passenger.ticket === "firstClass") {
+        orderFunction = function() {
+            alert("Would you like wine or a cocktail?");
+        };
+    } else if (passenger.ticket === "premium") {
+        orderFunction = function() {
+            alert("Would you like water, cola or wine?");
+        };
+    } else { // defaults to coach
+        orderFunction = function() {
+            alert("Would you like water or cola?");
+        };
+    }
+    return orderFunction;
+}
+
+// main function to Serve Passengers
+function servePassengers(passengers) {
+    for (var i = 0; i < passengers.length; i++) {
+        serveCustomer(passengers[i]);
+    }
+}
+
+// function to Serve Customer
+function serveCustomer(passenger) {
+    var getDrinkOrderFunction = createDrinkOrder(passenger);
+    getDrinkOrderFunction();
+    // get dinner order
+    getDrinkOrderFunction();
+    getDrinkOrderFunction();
+    // show movie
+    getDrinkOrderFunction();
+    // pick up trash
+}
+
 // print passenger's names and paid status
 function printPassengerInfo(passengerObj) {
     if(passengerObj.paid) {
-        console.log(passengerObj.name + " has paid.");
+        console.log(passengerObj.name + " has paid their " + passengerObj.ticket + " ticket.");
     } else {
-        console.log(passengerObj.name + " has not paid.");
+        console.log(passengerObj.name + " has not paid their " + passengerObj.ticket + " ticket.");
     }
 }
 
@@ -30,6 +69,7 @@ function checkNotPaid(passengerObj) {
     return (!passengerObj.paid);
 }
 
+/*
 var allCanFly = processPassengers(passengers, checkNoFlyList);
 if(!allCanFly) {
     console.log("The plane can't take off: we have a passenger on the no-fly list.");
@@ -39,5 +79,7 @@ var allPaid = processPassengers(passengers, checkNotPaid);
 if(!allPaid) {
     console.log("The plane can't take off: not everyone has paid.");
 }
+*/
 
-processPassengers(passengers, printPassengerInfo);
+// processPassengers(passengers, printPassengerInfo);
+servePassengers(passengers);
